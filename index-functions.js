28a35672bbb2fe4e1908c3901b299496d0b62bd9
atheel-alghamdi <atheel.1418h@gmail.com>
2022-02-20 16:44:41 +0300

    java updates

--- index-functions.js
+++ index-functions.js
@@ -1,16 +1,18 @@
+// Create a "close" button and append it to each list item
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
-  span.className = "close";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
 
+
+
 // Create a new list item when clicking on the "Add" button
 function newElement() {
-  var li = document.createElement("li");
+  var li = document.createElement("myUL");
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
@@ -18,15 +20,15 @@ function newElement() {
     alert("You must write something!");
   } else {
     document.getElementById("myUL").appendChild(li);
+    
   }
   document.getElementById("myInput").value = "";
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
-  span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
-
+  
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
       var div = this.parentElement;
