26073fb6d614344f91346a9f8f9deecac0e918f4
atheel-alghamdi <atheel.1418h@gmail.com>
2022-02-21 01:50:46 +0300

    updates

--- index-functions.js
+++ index-functions.js
@@ -1,18 +1,6 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
-
-
 // Create a new list item when clicking on the "Add" button
 function newElement() {
-  var li = document.createElement("myUL");
+  var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
@@ -24,8 +12,8 @@ function newElement() {
   }
   document.getElementById("myInput").value = "";
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+  var span = document.createElement("");
+  var txt = document.createTextNode("");
   span.appendChild(txt);
   li.appendChild(span);
   
